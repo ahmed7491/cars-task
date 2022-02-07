5d812c6d3dfe6c55db6f3863b30974907192501a
Bishoy Ibrahim <bishoyzy@gmail.com>
2022-02-07 09:50:08 +0200

    emp 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import { Button, Typography } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 
 interface CarModel {
@@ -18,7 +24,7 @@ const futureCars: CarModel[] = [
   },
   {
     id: 3,
-    name: 'Toyota' 
+    name: 'Toyota'
   },
   {
     id: 4,
@@ -50,18 +56,84 @@ const futureCars: CarModel[] = [
   }
 ]
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    formControl: {
+      margin: theme.spacing(3),
+    },
+  }),
+);
 
 function App() {
+  const defaultSelected = [7, 8];
+  const [initialChecked, setInitialChecked] = useState(defaultSelected);
+  const [initialList, setInitialList] = useState(futureCars);
+  const [applyButtonAppear, setApplyButtonAppear] = useState(false);
+  const classes = useStyles();
+
+  const handleChange = (id: any) => {
+    setApplyButtonAppear(true);
+    if (initialChecked.includes(id))
+      setInitialChecked(initialChecked.filter(item => +item !== +id));
+    else
+      setInitialChecked([...initialChecked, id]);
+  };
+
+  useEffect(() => {
+    if (initialChecked.sort().toString() == defaultSelected.sort().toString()) {
+      setApplyButtonAppear(false);
+      setInitialList(futureCars);
+    }
+  }, [initialChecked])
+
+  const resetFunc = () => {
+    setInitialChecked([7, 8]);
+    setInitialList(futureCars);
+    setApplyButtonAppear(false);
+  }
+
+  const applyChanges = () => {
+    const Checked = initialList.filter(item => initialChecked.find(itemm => item.id === itemm))
+    const UnChecked = initialList.filter(item => !Checked.includes(item))
+    setInitialList([...Checked, ...UnChecked]);
+  }
 
   return (
     <div className="App">
 
-      <header className="App-header">
+      <header>
         <h2>Future Cars</h2>
       </header>
-
-
-     
+      <div className='button_div'>
+        <Button onClick={resetFunc} variant="outlined" color="secondary">RESET</Button>
+        {applyButtonAppear &&
+          <Button onClick={applyChanges} variant="outlined" color="primary">APPLY CHANGES</Button>}
+      </div>
+      <div className="table">
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormGroup>
+            {initialList.map((item) => {
+              return (
+                <div key={item.id} className="button_div">
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={initialChecked.includes(item.id)}
+                        onChange={() => handleChange(item.id)}
+                        name={item.name} />
+                    }
+                    label={item.name}
+                  />
+                  <Typography variant="body2">{item.id}</Typography>
+                </div>
+              )
+            })}
+          </FormGroup>
+        </FormControl>
+      </div>
     </div>
   );
 }
